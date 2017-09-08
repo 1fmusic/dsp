9874ab0349e544f0c4b51a735dc8440bdc920f77
summer <summer@monksMacPro.hsd1.md.comcast.net>
2017-09-08 16:48:24 -0400

    adv python regex

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,18 @@
-PLACE YOUR CODE HERE
+import re
+
+def unique_domains(emails):
+    x = []
+    domains = []
+    
+    for z in emails:
+        z = z.strip('')
+        domain = re.findall('@(\S.*)',z)
+        x.append(domain)
+    
+    for i in x:    
+        if i not in domains :
+            domains.append(str(i))
+        else :
+            continue
+    
+    return domains
