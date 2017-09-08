dd23db59f013a509fead2844ef2cfb23e952d98c
summer <summer@monksMacPro.hsd1.md.comcast.net>
2017-09-08 16:51:05 -0400

    adv pyth dict code

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,26 @@
-PLACE YOUR CODE HERE
+import csv
+
+def get_dict2(filename):
+    deg = []
+    aff = []
+    email = []
+    name = ()
+    names ={} #create dictionary of last names and info
+    
+    with open(filename,'r') as fb :
+        reader = csv.reader(fb, delimiter=',', quotechar='"')
+        next(reader, None)  # skip the headers
+        data = [row for row in reader]
+        
+    for n in range(len(data)):
+        person = data[n]
+        name += (person[0].split(),)
+        deg.append(person[1])
+        aff.append(person[2])
+        email.append(person[3])
+          
+    for i in range(len(deg)): 
+        names[tuple(name[i])] = names.get(tuple(name[i]),[deg[i],aff[i],email[i]])
+
+    return names
+    
